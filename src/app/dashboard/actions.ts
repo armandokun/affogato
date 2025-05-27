abf20b4cbabcadc0b1f3a7e7841b8cd3350b47ec
armandokun <armandasvaicikauskas@protonmail.com>
2025-05-27 19:03:10 +0300

    Make title prompt make titles no more than 5 words

--- src/app/dashboard/actions.ts
+++ src/app/dashboard/actions.ts
@@ -10,9 +10,10 @@ export async function generateTitleFromUserMessage({
     model: myProvider.languageModel("openai-title-model"),
     system: `\n
     - you will generate a short title based on the first message a user begins a conversation with
-    - ensure it is not more than 80 characters long
+    - ensure it is not more than 5 words long
     - the title should be a summary of the user's message
-    - do not use quotes or colons`,
+    - do not use quotes or colons
+    - do not use any other characters than letters and spaces`,
     prompt: JSON.stringify(message),
   });
 
