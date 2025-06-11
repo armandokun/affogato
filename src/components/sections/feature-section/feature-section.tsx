ca37188fbe327c855d2f1c4c4a89ac0a156acd74
armandokun <armandasvaicikauskas@protonmail.com>
2025-06-11 19:15:47 +0300

    Fix text on smaller breakpoints

--- src/components/sections/feature-section/feature-section.tsx
+++ src/components/sections/feature-section/feature-section.tsx
@@ -36,13 +36,15 @@ const FeatureSection = () => {
       <div className="container">
         <div className="grid grid-cols-1 gap-24 mt-10 max-w-4xl mx-auto">
           <div className="flex flex-col items-center justify-center">
-            <h2 className="text-4xl font-medium mb-2 text-balance max-w-3xl mx-auto tracking-tighter">
-              Deep thinking Plus+
-            </h2>
-            <p className="text-gray-400 w-[80%] mx-auto mb-6">
-              AI models take more time to think and can use tools, like Web
-              Search to make better decisions.
-            </p>
+            <div className="flex flex-col items-center justify-center text-center w-[80%] mx-auto mb-6">
+              <h2 className="text-4xl font-medium mb-2 text-balance tracking-tighter">
+                Deep thinking Plus+
+              </h2>
+              <p className="text-gray-400">
+                AI models take more time to think and can use tools, like Web
+                Search to make better decisions.
+              </p>
+            </div>
             <VideoOnScroll
               src="/feature-videos/reasoning-web.mp4"
               muted
@@ -53,13 +55,15 @@ const FeatureSection = () => {
             />
           </div>
           <div className="flex flex-col items-center justify-center">
-            <h2 className="text-4xl font-medium mb-2 text-balance max-w-3xl mx-auto tracking-tighter">
-              Images, documents, you name it.
-            </h2>
-            <p className="text-gray-400 w-[80%] mx-auto mb-6">
-              The models can use images and documents to get more context to get
-              the best result.
-            </p>
+            <div className="flex flex-col items-center justify-center text-center w-[80%] mx-auto mb-6">
+              <h2 className="text-4xl font-medium mb-2 text-balance tracking-tighter">
+                Images, documents, you name it.
+              </h2>
+              <p className="text-gray-400">
+                The models can use images and documents to get more context to
+                get the best result.
+              </p>
+            </div>
             <VideoOnScroll
               src="/feature-videos/files.mp4"
               muted
@@ -70,13 +74,15 @@ const FeatureSection = () => {
             />
           </div>
           <div className="flex flex-col items-center justify-center">
-            <h2 className="text-4xl font-medium mb-2 text-balance max-w-3xl mx-auto tracking-tighter">
-              One conversation, multiple AI minds - combined.
-            </h2>
-            <p className="text-gray-400 w-[80%] mx-auto mb-6">
-              Use multiple AI models at once and enable features other models
-              don&apos;t support.
-            </p>
+            <div className="flex flex-col items-center justify-center text-center w-[80%] mx-auto mb-6">
+              <h2 className="text-4xl font-medium mb-2 text-balance tracking-tighter">
+                One conversation, multiple AI minds.
+              </h2>
+              <p className="text-gray-400">
+                Use multiple AI models at once and enable features other models
+                don&apos;t support.
+              </p>
+            </div>
             <VideoOnScroll
               src="/feature-videos/multiple-ai.mp4"
               muted
