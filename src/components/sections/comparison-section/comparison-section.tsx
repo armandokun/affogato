cd5d011caa2c624083937aaf7c8f7c08ba8297f0
armandokun <armandasvaicikauskas@protonmail.com>
2025-06-16 14:13:14 +0300

    Replace login link with pricing in comparison section

--- src/components/sections/comparison-section/comparison-section.tsx
+++ src/components/sections/comparison-section/comparison-section.tsx
@@ -145,7 +145,7 @@ const ComparisonSection = ({ currency }: { currency: "usd" | "eur" }) => {
 
         <div className="flex items-center justify-center mt-10">
           <Link
-            href={LOGIN}
+            href="#pricing"
             className="bg-secondary h-9 flex items-center justify-center text-sm font-normal tracking-wide rounded-full text-secondary-foreground w-fit px-4 shadow-[inset_0_1px_2px_rgba(255,255,255,0.25),0_3px_3px_-1.5px_rgba(16,24,40,0.06),0_1px_1px_rgba(16,24,40,0.08)] border border-white/[0.12] hover:bg-secondary/80 transition-all ease-out active:scale-95"
           >
             Buy Now
